d8625c9ea48179b45f9bde9e49642f9e5d462b26
TripathiRaghu <tripathiraghu1@gmail.com>
2020-07-05 18:03:38 +0530

    class and subject selection
    login class na subject selection process done

--- dashboard.php
+++ dashboard.php
@@ -22,7 +22,7 @@ include 'header.php';
 
 <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
       <script>window.jQuery || document.write('<script src="https://getbootstrap.com/docs/4.5/assets/js/vendor/jquery.slim.min.js"><\/script>')</script><script src="https://getbootstrap.com/docs/4.5/dist/js/bootstrap.bundle.min.js" integrity="sha384-1CmrxMRARb6aLqgBO7yyAxTOQE2AKb9GfXnEo760AUcUmFx3ibVJJAzGytlQcNXd" crossorigin="anonymous"></script>
-<script src="js/jquery.js"></script>loadData
+<script src="js/jquery.js"></script>
 <script type="text/javascript">
 	$(document).ready(function(){
 		function loadData(type, catagory_id){
@@ -45,7 +45,12 @@ include 'header.php';
 
 			$("#class").on("change",function(){
 				var className = $("#class").val();
+
+				if (className != "") {
 				loadData("subjectData", className );
+			}else{
+				$("#subject").html("");
+			}
 			})
 	});
 </script>
